0dc86ee9e30550ad71a955a5b7d5d0250e9a125f
masterpwned <grant.w@aol.com>
2016-03-16 19:36:05 -0700

    Initial Upload

---
+++ Chat Server/src/Chatter.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Chatter extends Thread {
+	Main cs;
+	Socket o;
+	boolean running = true;
+	
+	public Chatter(Main chatterServer, Socket outlit) {
+	   cs = chatterServer;
+	   o = outlit;
+	}
+	
+	
+	public boolean onoff() {
+		return running;
+	}
+	public void run() {
+		BufferedReader in = getBufferedReader(o);
+		while(running) {
+			try {
+				cs.sendMessage(in.readLine());
+			} catch (IOException e) {
+				running = false;
+			}
+		}		
+	}
+	
+	public void sender(String message) {
+			PrintWriter out = getPrintWriter(o);
+			out.println(message);
+	}
+
+	public static BufferedReader getBufferedReader(Socket clientSocket) {
+		try {
+			return new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+			return null;			
+		}
+	}
+	
+	public static PrintWriter getPrintWriter(Socket clientSocket) {
+		try {
+		    return new PrintWriter(clientSocket.getOutputStream(), true);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+			return null;
+		}
+	}
+}
